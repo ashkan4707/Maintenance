7883d9aca02e674eb6771ce031f4f4b7c3512e88
Ashkan Ghasemi <74142399+ashkan4707@users.noreply.github.com>
2023-02-12 21:05:35 +0400

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -24,13 +24,14 @@ do
 
 	IP=$(jq -r .result[$i].content records.json) && RE=$(telnet $IP 22)
 
-		if [[ " $RE " =~ "Unable to connect" ]]; then
- 			echo "$IP removed" && RECORDID=$(jq -r .result[$i].id records.json)&&curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$ZONE/dns_records/$RECORDID" \
-   				  -H "X-Auth-Email: hesam.imam@gmail.com" \
-   				  -H "X-Auth-Key: e37d986b5aeb10c07af376fd83aa90cf165c8" \
-				  -H "Content-Type: application/json"
-				
-
-			fi
+		if [[ " $RE " =~ "Unable to connect" ]]; 
+		then
+ 			echo "$IP removed" && RECORDID=$(jq -r .result[$i].id records.json)&&echo "$RECORDID request sent #1"
+		fi
+		
+		if [[ " $RE " =~ "could not resolve" ]]; 
+		then
+ 			echo "$IP removed" && RECORDID=$(jq -r .result[$i].id records.json)&&echo "$RECORDID request sent #2"
+		fi
 
 done
