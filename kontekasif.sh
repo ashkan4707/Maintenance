d45d0364256dff59a538f61a72691f47a5fae5ba
Ashkan Ghasemi <74142399+ashkan4707@users.noreply.github.com>
2023-05-06 21:53:55 +0400

    Update kontekasif.sh

--- kontekasif.sh
+++ kontekasif.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 
-# Create config file if it doesn't exist
-if [ ! -f kontekasifconfig ]; then
-  touch kontekasifconfig
-  read -p "Enter time to wait between requests (in seconds): " time
+# Define the log file
+log_file="kontekasiflogs"
+
+# Define the config file
+config_file="kontekasifconfig"
+
+# Check if the config file exists
+if [ -f "$config_file" ]; then
+  # Load the configuration data from the config file
+  . "$config_file"
+else
+  # Read the configuration data from the user
+  read -p "Enter the time interval in seconds: " time_interval
   read -p "Enter domain1: " domain1
   read -p "Enter domain2: " domain2
   read -p "Enter domain3: " domain3
-  read -p "Enter Cloudflare zone identifier: " zone_identifier
-  read -p "Enter Cloudflare CNAME identifier: " cname_identifier
-  read -p "Enter Cloudflare email: " email
-  read -p "Enter Cloudflare API Secret ID: " api_secret_id
-  echo "time=$time" > kontekasifconfig
-  echo "domain1=$domain1" >> kontekasifconfig
-  echo "domain2=$domain2" >> kontekasifconfig
-  echo "domain3=$domain3" >> kontekasifconfig
-  echo "zone_identifier=$zone_identifier" >> kontekasifconfig
-  echo "cname_identifier=$cname_identifier" >> kontekasifconfig
-  echo "email=$email" >> kontekasifconfig
-  echo "api_secret_id=$api_secret_id" >> kontekasifconfig
-fi
+  read -p "Enter zone identifier: " zone_identifier
+  read -p "Enter cname identifier: " cname_identifier
+  read -p "Enter email: " email
+  read -p "Enter API secret ID: " api_secret_id
 
-# Read config file
-source kontekasifconfig
-
-# Create log file if it doesn't exist
-if [ ! -f kontekasiflogs ]; then
-  touch kontekasiflogs
+  # Write the configuration data to the config file
+  cat << EOF > "$config_file"
+time_interval=$time_interval
+domain1=$domain1
+domain2=$domain2
+domain3=$domain3
+zone_identifier=$zone_identifier
+cname_identifier=$cname_identifier
+email=$email
+api_secret_id=$api_secret_id
+EOF
 fi
 
 # Add script to startup
@@ -34,136 +39,96 @@ fi
 
 # Infinite loop
 while true; do
-  # Wait for specified time
-  sleep $time
-  
-  # Get system time
-  system_time=$(date +"%Y-%m-%d %H:%M:%S")
-  
-  # Create and execute curl for domain1
-  curl_command=$(echo "curl --request PUT \
-    --url https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$cname_identifier \
-    --header 'Content-Type: application/json' \
-    --header 'X-Auth-Email: $email' \
-    --header 'X-Auth-Key: $api_secret_id' \
-    --data '{\"content\": \"$domain1.\", \"name\": \"@\", \"proxied\": false, \"type\": \"CNAME\", \"comment\": \"Last change $system_time\", \"ttl\": 1}'")
-  curl_response=$(eval $curl_command)
-  echo "$(date +"%Y-%m-%d %H:%M:%S") - Domain1 response: $curl_response" >> kontekasiflogs
-  
-  # Wait for specified time
-  sleep $time
+  # Get the system time
+  system_time=$(date '+%Y-%m-%d %H:%M:%S')
 
-  system_time=$(date +"%Y-%m-%d %H:%M:%S")
-  
-  # Create and execute curl for domain2
-  curl_command=$(echo "curl --request PUT \
-    --url https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$cname_identifier \
-    --header 'Content-Type: application/json' \
-    --header 'X-Auth-Email: $email' \
-    --header 'X-Auth-Key: $api_secret_id' \
-    --data '{\"content\": \"$domain2.\", \"name\": \"@\", \"proxied\": false, \"type\": \"CNAME\", \"comment\": \"Last change $system_time\", \"ttl\": 1}'")
-  curl_response=$(eval $curl_command)
-  echo "$(date +"%Y-%m-%d %H:%M:%S") - Domain2 response: $curl_response" >> kontekasiflogs
-  
-  # Wait for specified time
-  sleep $time
+  # Create the curl for domain1
+  curl_domain1=$(cat << EOF
+curl --request PUT \
+  --url https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$cname_identifier \
+  --header 'Content-Type: application/json' \
+  --header 'X-Auth-Email: $email' \
+  --header 'X-Auth-Key: $api_secret_id' \
+  --data '{
+  "content": "$domain1",
+  "name": "@",
+  "proxied": false,
+  "type": "CNAME",
+  "comment": "Last change $system_time",
+  "ttl": 1
+}'
+EOF
+)
 
-  system_time=$(date +"%Y-%m-%d %H:%M:%S")
-  
-  # Create and execute curl for domain3
-  curl_command=$(echo "curl --request PUT \
-    --url https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$cname_identifier \
-    --header 'Content-Type: application/json' \
-    --header 'X-Auth-Email: $email' \
-    --header 'X-Auth-Key: $api_secret_id' \
-    --data '{\"content\": \"$domain3.\", \"name\": \"@\", \"proxied\": false, \"type\": \"CNAME\", \"comment\": \"Last change $system_time\", \"ttl\": 1}'")
-  curl_response=$(eval $curl_command)
-  echo "$(date +"%Y-%m-%d %H:%M:%S") - Domain3 response: $curl_response" >> kontekasiflogs
-  
-  # Wait for specified time
-  sleep $time
-done#!/bin/bash
+  # Log the curl command for domain1
+  echo "[$system_time] Executing curl command for domain1: $curl_domain1" >> "$log_file"
 
-# Create config file if it doesn't exist
-if [ ! -f kontekasifconfig ]; then
-  touch kontekasifconfig
-  read -p "Enter time to wait between requests (in seconds): " time
-  read -p "Enter domain1: " domain1
-  read -p "Enter domain2: " domain2
-  read -p "Enter domain3: " domain3
-  read -p "Enter Cloudflare zone identifier: " zone_identifier
-  read -p "Enter Cloudflare CNAME identifier: " cname_identifier
-  read -p "Enter Cloudflare email: " email
-  read -p "Enter Cloudflare API Secret ID: " api_secret_id
-  echo "time=$time" > kontekasifconfig
-  echo "domain1=$domain1" >> kontekasifconfig
-  echo "domain2=$domain2" >> kontekasifconfig
-  echo "domain3=$domain3" >> kontekasifconfig
-  echo "zone_identifier=$zone_identifier" >> kontekasifconfig
-  echo "cname_identifier=$cname_identifier" >> kontekasifconfig
-  echo "email=$email" >> kontekasifconfig
-  echo "api_secret_id=$api_secret_id" >> kontekasifconfig
-fi
+  # Execute the curl command for domain1 and log the response
+  curl_response_domain1=$(eval "$curl_domain1")
+  echo "[$system_time] Response for domain1: $curl_response_domain1" >> "$log_file"
 
-# Read config file
-source kontekasifconfig
+  # Wait for the time interval
+  sleep "$time_interval"
 
-# Create log file if it doesn't exist
-if [ ! -f kontekasiflogs ]; then
-  touch kontekasiflogs
-fi
+# Get the system time
+  system_time=$(date '+%Y-%m-%d %H:%M:%S')
 
-# Add script to startup
-(crontab -l 2>/dev/null; echo "@reboot /path/to/script.sh") | crontab -
+  # Create the curl for domain2
+  curl_domain2=$(cat << EOF
+curl --request PUT \
+  --url https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$cname_identifier \
+  --header 'Content-Type: application/json' \
+  --header 'X-Auth-Email: $email' \
+  --header 'X-Auth-Key: $api_secret_id' \
+  --data '{
+  "content": "$domain2",
+  "name": "@",
+  "proxied": false,
+  "type": "CNAME",
+  "comment": "Last change $system_time",
+  "ttl": 1
+}'
+EOF
+)
 
-# Infinite loop
-while true; do
-  # Wait for specified time
-  sleep $time
-  
-  # Get system time
-  system_time=$(date +"%Y-%m-%d %H:%M:%S")
-  
-  # Create and execute curl for domain1
-  curl_command=$(echo "curl --request PUT \
-    --url https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$cname_identifier \
-    --header 'Content-Type: application/json' \
-    --header 'X-Auth-Email: $email' \
-    --header 'X-Auth-Key: $api_secret_id' \
-    --data '{\"content\": \"$domain1.\", \"name\": \"@\", \"proxied\": false, \"type\": \"CNAME\", \"comment\": \"Last change $system_time\", \"ttl\": 1}'")
-  curl_response=$(eval $curl_command)
-  echo "$(date +"%Y-%m-%d %H:%M:%S") - Domain1 response: $curl_response" >> kontekasiflogs
-  
-  # Wait for specified time
-  sleep $time
+  # Log the curl command for domain2
+  echo "[$system_time] Executing curl command for domain2: $curl_domain2" >> "$log_file"
 
-  system_time=$(date +"%Y-%m-%d %H:%M:%S")
-  
-  # Create and execute curl for domain2
-  curl_command=$(echo "curl --request PUT \
-    --url https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$cname_identifier \
-    --header 'Content-Type: application/json' \
-    --header 'X-Auth-Email: $email' \
-    --header 'X-Auth-Key: $api_secret_id' \
-    --data '{\"content\": \"$domain2.\", \"name\": \"@\", \"proxied\": false, \"type\": \"CNAME\", \"comment\": \"Last change $system_time\", \"ttl\": 1}'")
-  curl_response=$(eval $curl_command)
-  echo "$(date +"%Y-%m-%d %H:%M:%S") - Domain2 response: $curl_response" >> kontekasiflogs
-  
-  # Wait for specified time
-  sleep $time
+  # Execute the curl command for domain2 and log the response
+  curl_response_domain2=$(eval "$curl_domain2")
+  echo "[$system_time] Response for domain2: $curl_response_domain2" >> "$log_file"
 
-  system_time=$(date +"%Y-%m-%d %H:%M:%S")
-  
-  # Create and execute curl for domain3
-  curl_command=$(echo "curl --request PUT \
-    --url https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$cname_identifier \
-    --header 'Content-Type: application/json' \
-    --header 'X-Auth-Email: $email' \
-    --header 'X-Auth-Key: $api_secret_id' \
-    --data '{\"content\": \"$domain3.\", \"name\": \"@\", \"proxied\": false, \"type\": \"CNAME\", \"comment\": \"Last change $system_time\", \"ttl\": 1}'")
-  curl_response=$(eval $curl_command)
-  echo "$(date +"%Y-%m-%d %H:%M:%S") - Domain3 response: $curl_response" >> kontekasiflogs
-  
-  # Wait for specified time
-  sleep $time
-done
+  # Wait for the time interval
+  sleep "$time_interval"
+
+# Get the system time
+  system_time=$(date '+%Y-%m-%d %H:%M:%S')
+
+  # Create the curl for domain3
+  curl_domain3=$(cat << EOF
+curl --request PUT \
+  --url https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$cname_identifier \
+  --header 'Content-Type: application/json' \
+  --header 'X-Auth-Email: $email' \
+  --header 'X-Auth-Key: $api_secret_id' \
+  --data '{
+  "content": "$domain3",
+  "name": "@",
+  "proxied": false,
+  "type": "CNAME",
+  "comment": "Last change $system_time",
+  "ttl": 1
+}'
+EOF
+)
+
+  # Log the curl command for domain3
+  echo "[$system_time] Executing curl command for domain3: $curl_domain3" >> "$log_file"
+
+  # Execute the curl command for domain3 and log the response
+  curl_response_domain3=$(eval "$curl_domain3")
+  echo "[$system_time] Response for domain3: $curl_response_domain3" >> "$log_file"
+
+  # Wait for the time interval
+  sleep "$time_interval"
+done >> kontekasiflogs 2>&1 &
